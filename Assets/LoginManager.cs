c4b7d4ab84f5aca562d189f62998036e075aeee3
UriZaltzman <144272394+UriZaltzman@users.noreply.github.com>
2024-05-27 15:49:09 -0300

    Añadido la contra de acceso denegado o acceso permitido

--- Assets/LoginManager.cs
+++ Assets/LoginManager.cs
@@ -1,18 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+//Cuando el usuario presione el BtnAceptar
+//debe escribirse un mensaje en la consola
+//si el texto delinputPassword coincide
+//con una conrtaseña predefinida por ustedes
+//escribir "Acces Granted"
+//sino escribir "Acces Denied"
 public class LoginManager : MonoBehaviour
 {
+    public InputField inputPassword;
+    public Text mensajeAcceso;
+    string contraseñaPredefinida = "miContraseña";
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void OnClickBtnAceptar()
+    {
+        string contraseñaIngresada = inputPassword.text;
+
+        if (contraseñaIngresada == contraseñaPredefinida)
+        {
+            mensajeAcceso.text = "Acceso Concedido";
+            Debug.Log("Acceso Concedido");
+        }
+        else
+        {
+            mensajeAcceso.text = "Acceso Denegado";
+            Debug.Log("Acceso Denegado");
+        }
     }
 }
